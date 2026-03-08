b40bfac1b4ddb245c8ee983336319e53ecafe540
mrdeyroy <shibamdeyroy02@gmail.com>
2026-03-08 16:35:09 +0530

    fix render port binding

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ import app from "./api/index.js";
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "production") {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
