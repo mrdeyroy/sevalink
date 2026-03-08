f172ae7dae5f9f6a66fb7c2542268bf73f7c629b
mrdeyroy <shibamdeyroy02@gmail.com>
2026-03-08 17:57:45 +0530

    callback url

--- server/config/passport.js
+++ server/config/passport.js
@@ -11,7 +11,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: "/api/auth/google/callback",
+                callbackURL: "https://sevalink-backend-76zq.onrender.com/api/auth/google/callback",
             },
             async (accessToken, refreshToken, profile, done) => {
                 try {
